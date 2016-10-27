b57d3a8fca9847c4883a6e50d113f4239f686585
Jakob Lind <karl.jakob.lind@gmail.com>
2016-10-27 10:25:36 +0200

    add some tests

---
+++ tests/js-import-test.el
@@ -0,0 +1,36 @@
+;;; js-import-test.el --- Test for js-import         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Jakob Lind
+
+;; Author: Jakob Lind <karl.jakob.lind@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'ert)
+(require 'js-import)
+
+(ert-deftest load-package-json ()
+  "Test loading of package-json file"
+  (should (equal (js-import-get-project-dependencies "not-exist") nil))
+  (should (equal (js-import-get-project-dependencies "./package.json.empty.dependencies") nil))
+  (should (equal (js-import-get-project-dependencies "package.json") '("redux" "react"))))
+
+;;; js-import-test.el ends here
